7adf82f52833305f57883069ef139ba1ff4244c7
njdepsky <nicholas.depsky@gmail.com>
2018-04-27 17:08:01 -0700

    AddedDemandPriorities

--- helper_functions.jl
+++ helper_functions.jl
@@ -4,10 +4,10 @@
 ##  output:  the node dictionary
 function create_demand_node(; node_type::String = "default_type", name::String = "default_name", 
     size::Float64= -999., rate::Float64 = -999., size_units::String = "default_units", 
-    demand_units::String = "demand_units");
+    demand_units::String = "demand_units", priority::Int64 = 999);
 
     return Dict("name" => name, "size" => size, "rate" => rate,    
-    "size_units" => size_units, "demand_units" => demand_units); 
+    "size_units" => size_units, "demand_units" => demand_units, priority => priority); 
 end
 
 ##  function to add a supply node to the list of suppl nodes
