57e838c1ace595c36cb6eb43e11f2c7e02555085
lrennels <lrennels@berkeley.edu>
2018-05-11 18:58:22 -0700

    Fix Merge Conflict

--- Final_Project.jl
+++ Final_Project.jl
@@ -2,8 +2,8 @@
 ## Lisa Rennels and Nick Depsky
 ## ER 290A 
 
-#cd("/Users/nicholasdepsky/Dropbox/Berkeley_tings/Spring 2018/ER 290A/ER290A-finalproject")
-cd("/Users/lisarennels/Documents/UC Berkeley ERG/ENERES 290A/Final Project/ER290A-finalproject")
+cd("/Users/nicholasdepsky/Dropbox/Berkeley_tings/Spring 2018/ER 290A/ER290A-finalproject")
+#cd("/Users/lisarennels/Documents/UC Berkeley ERG/ENERES 290A/Final Project/ER290A-finalproject")
 
 ####-- DEFINE DATE PERIOD --------------------------------------------------####
 const start_year = 1990
@@ -15,37 +15,27 @@ const modays = [31, 28, 31, 30 ,31, 30, 31, 31, 30 ,31, 30 , 31]
 include("helper_functions.jl");
 
 ####-- DEFINE SUPPLY -------------------------------------------------------####
-#returns 
-#   - supply_nodes, a list of dictionaries defining the demand nodes
-#   - supply, a dataframe of the supply
-
+#returns demand_nodes, a list of dictionaries defining the demand nodes
 include("DefineSupply.jl")
+# Create Supply DataFrame
 supply = sdf(supply_nodes, start_year, stop_year)
 
 ####-- DEFINE DEMAND -------------------------------------------------------####
-#returns 
-#   - demand_nodes, a list of dictionaries defining the demand nodes
-#   - demand, a dataframe of the supplyinclude("DefineDemand.jl")
-
+#returns supply_nodes, a list of dictionaries defining the supply nodes
 include("DefineDemand.jl")
 demand = ddf(demand_nodes, start_year, stop_year)
 
 ####-- DEFINE INFRASTRUCTURE -----------------------------------------------####
-#returns 
-#   - reservoir_nodes, a list of dictionaries defining the demand nodes
-#   - demand, a dataframe of the supplyinclude("DefineDemand.jl")
-
 include("DefineInfrastructure.jl")
+res_inits = res_init(reservoir_nodes)
 reservoirs = rdf(reservoir_nodes, start_year, stop_year)
 
 ####-- RUN DRIP ------------------------------------------------------------####
-include("DRIP_allocation_routine.jl")
-#returns 
-#   - results, a vector of demand fractions filled, one per demand node
-results = DRIP_allocation_routine(demand_nodes, supply_nodes, reservoir_nodes,
-(stop_year - start_year + 1));
+#include("DRIP_allocation_routine.jl")
+
+#results = DRIP_allocation(demand, supply, reservoirs,(stop_year - start_year + 1));
 
 ####-- RUN GRAPHICS---------------------------------------------------------####
-splot(supply)
-dplot(demand)
+splot(supply, 1990, 2017)
+dplot(demand, 1990, 1990)
 rplot(reservoirs)
