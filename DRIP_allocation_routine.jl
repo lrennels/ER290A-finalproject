cc2783a348bf560df2e6441f99f9c8e4afaa4ac5
lrennels <lrennels@berkeley.edu>
2018-05-11 18:25:00 -0700

    Create two versions of allocation routine; clean main script

--- DRIP_allocation_routine.jl
+++ DRIP_allocation_routine.jl
@@ -1,126 +1,12 @@
-## This file defines a WIP version of the DRIP allocation routine that involves
-## more complex algorithms and prepares for an optimization step.
+## This file defines a deterministic algorithm for DRIP, and can be used as a 
+#skeleton as we decide how to include optimization components 
 
-## ---------------------------------------------------------------------------##
-## WIP WORK ON OPTIMIZABLE MODEL ---------------------------------------------##
-## ---------------------------------------------------------------------------##
+## ASSUMPTION 1:  All supply is upstream of all demand - we may want to alter 
+## this with some sort of distance variable for all the nodes, but might be 
+## unnecesarily complicated for now
 
-#initialize variables
-res_tm1 = rcopy[:RESULTS_Storage][t-1]
-inflow = 0
-withd = 0
-dem = 0
-sup = 0
-
-#configure the Excel Optim model into Julia
-for l in 1:length(locs)
-
-    #configure model for first node
-    if l == 1
-        
-        if locs[l] in slocs # If supply node
-            if scopy[scopy[:Loc].==locs[l],:][:Units][t] == "CMS"
-                sup = scopy[scopy[:Loc].==locs[l],:][:Quantity][t]*60*60*24*mdays
-                [Dates.month(dates[t])] # convert to MM3
-            else
-                sup = scopy[scopy[:Loc].==locs[l],:][:Quantity][t]
-            end
-
-        elseif locs[l] in dlocs # If demand node
-            if dcopy[dcopy[:Loc].==locs[l],:][:Units][t] == "CMS"
-                dem = dcopy[dcopy[:Loc].==locs[l],:][:Quantity][t]*60*60*24*mdays
-                [Dates.month(dates[t])] # convert to MM3
-            else
-                dem = dcopy[dcopy[:Loc].==locs[l],:][:Quantity][t]
-            end
-
-        elseif locs[l] in rlocs # If reservoir node
-            if t == 1
-                sup = res_inits[find(rlocs .== locs[l])]
-            else
-                sup = rcopy[:RESULTS_Storage][t-1]
-            end           
-        end
-        
-    #not in the first location   
-    else
-
-    end
-end
-
-#create objective function
-function dem_covg_obj(withdrawal_frac)
-end
-
-# Function to determine how much water reservoir should store/release
-function resbal(t,l)
-end
-
-## ---------------------------------------------------------------------------##
-## ORIGINAL WORK, ENHANCED ----- ---------------------------------------------##
-## ---------------------------------------------------------------------------##
-
-function DRIP_allocation(demand, supply, reservoirs, res_inits, start_year, stop_year, year_ts = 12)
-
-    dcopy = deepcopy(demand)
-    dcopy[:RESULTS_DemandMet] = -999.
-    dcopy[:RESULTS_DemandUnmet] = -999.
-    dcopy[:RESULTS_PctCoverage] = -999.
-    dcopy[:RESULTS_PctShortage] = -999.
-
-    scopy = deepcopy(supply)
-
-    rcopy = deepcopy(reservoirs)
-    rcopy[:RESULTS_Inflow] = -999.
-    rcopy[:RESULTS_Outflow] = -999.
-
-    nyears = stop_year - start_year + 1
-    dates = Date.(sort(repmat(start_year:stop_year,12)),repmat(1:12,nyears))
-    lyears = 1800:4:2200
-
-    # find all locations of demand, supply, reservoir nodes (1 = furthest upstream)
-    locs = sort(unique(vcat(dcopy[:Loc], scopy[:Loc], rcopy[:Loc])))
-    slocs = unique(scopy[:Loc])
-    dlocs = unique(dcopy[:Loc])
-    rlocs = unique(rcopy[:Loc])
-
-    # create stream reach flows matrix (reprsents flow at end of reach)
-    reach_flow = fill(0.,length(dates),length(locs))
-
-    for t = 1:length(dates)
-        if (Dates.year(dates[t]) in lyears)
-            mdays = [31,29,31,30,31,30,31,31,30,31,30,31]
-        else
-            mdays = [31,28,31,30,31,30,31,31,30,31,30,31]
-        end
-
-        for l = 1:length(locs)
-            #Define Inflow from upstream reach
-            if (l == 1)
-                inflow = 0 # inflow from upstream reach is 0 for top reach
-            else
-                inflow = reach_flow[t,l-1] # takes streamflow 
-            end
-
-            ## Define withdrawals for demand nodes or reservoirs within reach
-            # If current node is not a demand or reservoir node, and is upstream of any reservoirs
-            if (!(locs[l] in dlocs) && locs[l] < minimum(rlocs))
-                deliv = 0
-                resb = 0
-            elseif ()
-
-            end
-            
-            #Define supply to reach
-            if (scopy[scopy[:Loc].==l,:][:Units][t] == "CMS")
-                sup = scopy[scopy[:Loc].==locs[l],:][:Quantity][t]*60*60*24*mdays[Dates.month(dates[t])]
-            else
-                sup = scopy[:Quantity][t]
-            end
-            reach_flow[t,l] = inflow + sup - deliv + resb
-        end
-    end
-end
+function DRIP_deterministic(demand_nodes::Array{}, supply_nodes::Array{}, 
+    reservoir_nodes::Array{}, numyears::Int64)
         
     #get maximum priority number
     max_demand_priority = 0
@@ -131,10 +17,14 @@ end
     #sort demand nodes
     demand_nodes= sort(demand_nodes, by = x -> x["priority"])
 
+    #track the total current supply
+    current_supply= 0.
+
     #track how much of each demand node gets demand_frac_filleded
     demand_frac_filled = zeros(length(demand_nodes))
 
     for m = 1:numyears * 12
+
         s_index = m
         m_index = m%12 
         if m_index == 0
@@ -143,15 +33,14 @@ end
 
         #add the monthly supply to the current_supply, assuming the supply is in
         #cubic feet per second
-        #track the total current supply
-        current_supply= 0.
         for node = 1:length(supply_nodes)
-            current_supply += supply_nodes[node]["inflow"][:CMS][s_index] * 60*60*24*modays[m_index]
+            current_supply += supply_nodes[node]["inflow"][:CFS][s_index] * 60*60*24*modays[m_index]
         end
 
-        #loop over the demand priorities
+        #loop over the demand
         current_node = 1
         for p = 1:max_demand_priority
+
             current_set = []
             while current_node <= length(demand_nodes) && demand_nodes[current_node]["priority"] == p
                 push!(current_set, demand_nodes[current_node])
@@ -182,5 +71,6 @@ end
                 
             end
         end
+    end
     return demand_frac_filled
 end
--- Final_Project.jl
+++ Final_Project.jl
@@ -2,8 +2,8 @@
 ## Lisa Rennels and Nick Depsky
 ## ER 290A 
 
-cd("/Users/nicholasdepsky/Dropbox/Berkeley_tings/Spring 2018/ER 290A/ER290A-finalproject")
-#cd("/Users/lisarennels/Documents/UC Berkeley ERG/ENERES 290A/Final Project/ER290A-finalproject")
+#cd("/Users/nicholasdepsky/Dropbox/Berkeley_tings/Spring 2018/ER 290A/ER290A-finalproject")
+cd("/Users/lisarennels/Documents/UC Berkeley ERG/ENERES 290A/Final Project/ER290A-finalproject")
 
 ####-- DEFINE DATE PERIOD --------------------------------------------------####
 const start_year = 1990
@@ -15,24 +15,33 @@ const modays = [31, 28, 31, 30 ,31, 30, 31, 31, 30 ,31, 30 , 31]
 include("helper_functions.jl");
 
 ####-- DEFINE SUPPLY -------------------------------------------------------####
-#returns demand_nodes, a list of dictionaries defining the demand nodes
+#returns 
+#   - supply_nodes, a list of dictionaries defining the demand nodes
+#   - supply, a dataframe of the supply
+
 include("DefineSupply.jl")
-# Create Supply DataFrame
 supply = sdf(supply_nodes, start_year, stop_year)
 
 ####-- DEFINE DEMAND -------------------------------------------------------####
-#returns supply_nodes, a list of dictionaries defining the supply nodes
+#returns 
+#   - demand_nodes, a list of dictionaries defining the demand nodes
+#   - demand, a dataframe of the supplyinclude("DefineDemand.jl")
+
 include("DefineDemand.jl")
 demand = ddf(demand_nodes, start_year, stop_year)
 
 ####-- DEFINE INFRASTRUCTURE -----------------------------------------------####
+#returns 
+#   - demand_nodes, a list of dictionaries defining the demand nodes
+#   - demand, a dataframe of the supplyinclude("DefineDemand.jl")
+
 include("DefineInfrastructure.jl")
-res_inits = res_init(reservoir_nodes)
 reservoirs = rdf(reservoir_nodes, start_year, stop_year)
 
 ####-- RUN DRIP ------------------------------------------------------------####
-#include("DRIP_allocation_routine.jl")
-
+include("DRIP_allocation_routine.jl")
+#returns 
+#   - results, a vector of demand fractions filled, one per demand node
 results = DRIP_allocation(demand, supply, reservoirs,(stop_year - start_year + 1));
 
 ####-- RUN GRAPHICS---------------------------------------------------------####
